eec99789a21530b9fa51504417eaaf15c2497b1f
Erasmo <erasmo.gomez@airnguru.com>
2025-09-16 12:50:37 -0500

    Update clase 04

---
+++ Clase04/LinkedList/main.cpp
@@ -0,0 +1,8 @@
+#include "src/BibliotecaLista.hpp"
+
+int main() {
+    struct Lista l;
+    construir(l);
+    cout<<l.size<<endl;
+    return 0;
+}
---
+++ Clase04/LinkedList/src/BibliotecaLista.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Erasmo on 12/09/25.
+//
+
+#ifndef LINKEDLIST_BIBLIOTECALISTA_HPP
+#define LINKEDLIST_BIBLIOTECALISTA_HPP
+#include "Lista.hpp"
+#include "Utils.hpp"
+void construir(struct Lista& l);
+bool esListaVacia(struct Lista& l);
+void insertar_inicio(struct Lista& l, struct Elemento & e);
+
+#endif //LINKEDLIST_BIBLIOTECALISTA_HPP
