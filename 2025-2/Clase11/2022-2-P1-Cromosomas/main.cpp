9af697a4779a08049ab1e8e6d2902b81b75dded0
Erasmo <erasmo.gomez@airnguru.com>
2025-11-10 16:54:04 -0500

    update clase 11

---
+++ 2025-2/Clase11/2022-2-P1-Cromosomas/main.cpp
@@ -0,0 +1,17 @@
+#include "src/BibliotecaArboles/ArbolBinario.hpp"
+#include "src/FuncionesArbol.hpp"
+#include "src/FuncionesCola.hpp"
+#include "src/Funciones.hpp"
+
+int main() {
+    int array[] = {10,50,20,30,40};
+    int peso = 70;
+
+    int n = sizeof(array) / sizeof(array[0]);
+    cout << n << endl;
+    ArbolBinario arbol = construir_arbol_arreglo(array, n);
+    recorrido_por_niveles(arbol);
+    int cantidad_combinaciones = contar_combinaciones(arbol, peso);
+    cout << "\nNúmero de combinaciones: " << cantidad_combinaciones << endl;
+    return 0;
+}
