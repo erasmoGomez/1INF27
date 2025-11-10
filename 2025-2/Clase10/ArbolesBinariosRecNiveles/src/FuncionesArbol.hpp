3a89821b3712d5022a3e60972453d3d338090649
Erasmo <erasmo.gomez@airnguru.com>
2025-11-10 15:56:37 -0500

    update clase 10 con comentarios

---
+++ 2025-2/Clase10/ArbolesBinariosRecNiveles/src/FuncionesArbol.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Erasmo on 7/11/25.
+//
+
+#ifndef ARBOLESBINARIOS_FUNCIONESARBOL_HPP
+#define ARBOLESBINARIOS_FUNCIONESARBOL_HPP
+
+#include "Utils.hpp"
+#include "BibliotecaArboles/ArbolBinario.hpp"
+#include "FuncionesCola.hpp"
+
+void construir(struct ArbolBinario &arbol);
+
+bool esArbolVacio(struct ArbolBinario &arbol);
+
+void plantarArbolBinario(ArbolBinario &arbol,
+                         const struct ArbolBinario &izq,
+                         const struct ElementoArbol &elemento,
+                         const struct ArbolBinario &der);
+
+void plantarNodoArbolBinario(struct NodoArbol *&nodo,
+                             struct NodoArbol *izq,
+                             const struct ElementoArbol &elemento,
+                             struct NodoArbol *der);
+
+void recorrer_in_order(struct ArbolBinario &arbol);
+
+void recorrer_pre_order(struct ArbolBinario &arbol);
+
+void recorrer_post_order(struct ArbolBinario &arbol);
+
+void recorrido_por_niveles(ArbolBinario &arbol);
+
+void bfs_rec(Cola &cola);
+
+void recorrido_por_niveles_recursivo(ArbolBinario &arbol);
+#endif //ARBOLESBINARIOS_FUNCIONESARBOL_HPP
